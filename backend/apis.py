ea22dedcbf48511eb14e5d3c936a8253e157ba19
benruckman <ben.ruckman@live.com>
2023-09-12 21:23:33 -0600

    Get players works - stubbed out get_players_from_leaderboard()

--- backend/apis.py
+++ backend/apis.py
@@ -1,15 +1,27 @@
-from pathlib import Path as P
+import asyncio
 
-from chessdotcom import get_player_profile
-
-chess_path = P(__file__).parent / "data" / "chess.com"
-
-
-def get_player(username):
-    return get_player_profile(username).json()
+import requests
+from chessdotcom.aio import ChessDotComResponse, Client, get_player_profile
 
 
-response = get_player_profile("fabianocaruana")
-chess_path.write_text(response.json())
+class ChessDotComClient(Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rate_limit_handler.tries = 2
+        self.rate_limit_handler.tts = 4
+        self.request_config["headers"]["User-Agent"] = "ben.ruckman@live.com"
 
-player_name = response.player.name
+    async def get_players(self, usernames: list[str]):
+        cors = [get_player_profile(name) for name in usernames]
+
+        async def gather_cors(cors: list[ChessDotComResponse]):
+            return await asyncio.gather(*cors)
+
+        responses = await gather_cors(cors)
+        return responses
+
+    # async def get_players_from_league(self, league_id: str):
+    #     CLient.
+
+    async def get_players_from_leaderboards(self):
+        pass
